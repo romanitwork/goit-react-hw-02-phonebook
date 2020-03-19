e7ab19c0fcf8009718a1da8414f8aa6fc805fd53
romanitwork <workromanmail@gmail.com>
2020-03-19 15:45:06 +0200

    Updates

---
+++ precache-manifest.64b182856862280b2ef8e9f94964ba22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29ed94a97f86a21f56328b685aa8145",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6f916b1e8b00b8aee079",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bf3fe0399212325f910",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7e5d0bd7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.7e5d0bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f916b1e8b00b8aee079",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.5b752ef3.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);
